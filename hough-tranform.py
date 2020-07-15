d41fec8b9455292a5d9d043268d1cd1d462ce9c4
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-07-15 16:32:21 -0400

    added hough demo + custom params for transform

--- hough-tranform.py
+++ hough-tranform.py
@@ -31,11 +31,11 @@ masked_edges = cv2.bitwise_and(edges, mask)
 
 # Define the Hough transform parameters
 # Make a blank the same size as our image to draw on
-rho = 1
-theta = np.pi / 180
-threshold = 1
-min_line_length = 10
-max_line_gap = 1
+rho = 2  # distance resolution in pixels of the Hough grid
+theta = np.pi / 180  # angular resolution in radians of the Hough grid
+threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+min_line_length = 15  # minimum number of pixels making up a line
+max_line_gap = 13
 line_image = np.copy(image) * 0  # creating a blank to draw lines on
 
 # Run Hough on edge detected image
